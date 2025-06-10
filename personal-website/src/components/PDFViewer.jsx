c872daa127b2ff353342825ab70cfacd4e6003f4
Justin <zlac478@live.rhul.ac.uk>
2025-06-10 15:15:34 +0100

    Fix whitespace removal solution for after each pdf page

--- personal-website/src/components/PDFViewer.jsx
+++ personal-website/src/components/PDFViewer.jsx
@@ -1,5 +1,6 @@
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 import { useState } from "react";
 
 //explicitly tell react where to find worker script
@@ -8,9 +9,18 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 function PDFViewer() {
   const [numPages, setNumPages] = useState(null); //keep track of the number of pages in the pdf, provide a method to update number of pages
 
+  const [width, setWidth] = useState(1000);
+
   //update the state to hold the number of pages in pdf
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
+  };
+
+  const pageBreakStyle = {
+    border: "none",
+    borderTop: "2px #aaa",
+    margin: "0.5rem 0",
+    width: "80%",
   };
 
   //return pdf component
@@ -18,11 +28,15 @@ function PDFViewer() {
     <Document file="/CV.pdf" onLoadSuccess={onDocumentLoadSuccess}>
       {numPages &&
         Array.from(new Array(numPages), (el, index) => (
-          <Page
-            key={`page_${index + 1}`}
-            pageNumber={index + 1}
-            renderTextLayer={false} //TextLayer rendering leads to a giant whitespace under each page, so it is disabled
-          />
+          <div key={`page_container_${index}`}>
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              width={width}
+              renderTextLayer={true}
+            />
+            {index < numPages - 1 && <hr style={pageBreakStyle} />}
+          </div>
         ))}
     </Document>
   );
