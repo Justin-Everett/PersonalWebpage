c69e18467ef7ae734d9d3ef221ba1e55c0ebc5d4
Justin <zlac478@live.rhul.ac.uk>
2025-06-13 14:12:27 +0100

    Fix routing for development

--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === "production" ? "/PersonalWebpage/" : "/",
-    plugins: [react()],
-  };
+export default defineConfig({
+  base: "/PersonalWebpage/",
+  plugins: [react()],
 });
